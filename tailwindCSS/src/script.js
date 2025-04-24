bcbed9865025f3b622a18d7f76501c8bcd289bcb
mayyank1 <mayankgauur@gmail.com>
2025-04-25 01:08:40 +0530

    cart added

--- tailwindCSS/src/script.js
+++ tailwindCSS/src/script.js
@@ -1,18 +1,26 @@
+let cart = 0;
+
+// hero section info
 document.getElementById("shop").addEventListener("click", function(){
-    alert("Shop now!!");
+    cart++;
+    document.getElementById("cartCount").textContent = cart;
     console.log("Shop clicked");
 });
 
+
+// product card info
 const nodelist = document.querySelectorAll("section > div > div > button");
 let nodelist1 = document.querySelectorAll("section >div>div>p");
 
 for(let i=0;i<nodelist.length;i++){
         nodelist[i].addEventListener("click" , function(){
-        alert("Shop");
+        cart++;
+        document.getElementById("cartCount").textContent = cart;
         console.log(nodelist1[i*2].textContent);
     });
 }
 
+// search bar
 document.getElementById("searchBar").addEventListener("input" , function(){
     value = this.value.toLowerCase();
     let product = document.querySelectorAll("section > div > div");
@@ -29,3 +37,6 @@ document.getElementById("searchBar").addEventListener("input" , function(){
         }
     });
 });
+
+// Cart dom manupulation
+// document.getElementById("cartCount").textContent = cart;
